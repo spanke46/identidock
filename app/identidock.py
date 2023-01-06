9bda01abf4ca80972a260abf6008d658f9ac95ef
spanke46 <77513109+spanke46@users.noreply.github.com>
2023-01-06 13:14:42 +0200

    Update identidock.py

--- app/identidock.py
+++ app/identidock.py
@@ -2,7 +2,7 @@ from flask import Flask, Response, request
 import requests
 import hashlib
 import redis
-
+#TEST
 app = Flask(__name__)
 cache = redis.StrictRedis(host='redis', port=6379, db=0)
 salt = "UNIQUE_SALT"
